9818e5638f8c96c80fa2412688792047b1097949
vaibh <vaibh@VaibhavPC>
2023-02-18 18:01:09 +0530

    v commit

--- src/main/java/com/amazon/classifieds/App.java
+++ src/main/java/com/amazon/classifieds/App.java
@@ -10,9 +10,11 @@ public class App
 		System.out.println( "************************************************" );
 		System.out.println( "Welcome to Amazon Internal  Classified App" );
 		System.out.println( "************************************************" );
+		
 		if(args.length>0) {
 			ConnectionManager.FILEPATH=args[0];
 		}
+		
 		AppDriver appDriver= new AppDriver();
 		appDriver.initiate();
 
